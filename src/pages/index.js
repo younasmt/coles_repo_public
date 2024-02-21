70e0e4aaa90102c5e680362bacbef29a2e039ebe
Tariq Younas <tariq.younas@gmail.com>
2024-02-21 21:12:16 +1100

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+
+const IndexPage = () => (
+  <Layout>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
