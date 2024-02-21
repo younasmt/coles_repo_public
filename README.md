53ea75c227b67ad484d42bc6f2907b7f0db72a50
Tariq Younas <tariq.younas@gmail.com>
2024-02-21 21:10:50 +1100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center">
+  Super Specials Sample
+</h1>
+
+## 🚀 Get going
+
+1.  **Install Nodejs and Gatsby**
+
+    You will need to install the following to be able to run and build the static content:
+
+    * nodejs
+    * gatsby cli
+
+    To install gatsby cli run the following:
+
+    ```shell
+    npm install -g gatsby-cli
+     ```
+
+2.  **Start the dev server**
+
+    Start the development server from this folder
+
+    ```shell
+    gatsby develop
+    ```
+
+3.  **Build the static site**
+
+    Run the following command to build the static site. The site will be built into the ./public folder
+
+      ```shell
+      gatsby build
+      ```
+
+    Once the site is built you can deploy it to a web server of some kind.
---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Super Specials`,
+    description: `Super specials deals just for you!`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#e01a22`,
+        theme_color: `#e01a22`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
